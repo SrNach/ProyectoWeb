a621836b280ad61c690ab1ee6c0233d36f58e51c
SrNach <89942238+SrNach@users.noreply.github.com>
2025-06-26 01:52:18 +0000

    .sql

--- ProyectoBurger/backend/DB/Usuarios.sql
+++ ProyectoBurger/backend/DB/Usuarios.sql
@@ -1,7 +1,13 @@
-CREATE TABLE `cliente` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE DATABASE IF NOT EXISTS Usuarios
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_general_ci;
+
+USE Usuarios;
+
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL,
-  `numero` int(11) NOT NULL,
+  `numero` BIGINT(11) NOT NULL,
   `direccion` varchar(100) NOT NULL,
   `correo` varchar(100) NOT NULL,
   `passw` varchar(100) NOT NULL,
@@ -9,7 +15,7 @@ CREATE TABLE `cliente` (
   UNIQUE KEY `correo_unico` (`correo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `cliente` (`nombre`, `numero`, `direccion`, `correo`, `passw`) VALUES
+INSERT IGNORE INTO `cliente` (`nombre`, `numero`, `direccion`, `correo`, `passw`) VALUES
 ('Ignacio Silva Rubio', 972727272, 'Calle falsisisma 123', 'ignacio.silva.r@mail.pucv.cl', '$2b$10$4Nt/mxoeMFMEro2cMx3T..Nx1oap1Io6RkNCXHRkYBdBIo6G0q8ly'),
 ('Davor Serey', 912231234, 'Calle realisiisisisisima 435', 'davor.serey.g@mail.pucv.cl', '$2b$10$z.2bvSNxYuRD9W/.AWmHQekMo2s5/dkMBGPXvvEsVC5CRlOLsTayS'),
 ('Don Test', 999999999, 'Avenida Brasil 567', 'test@test.com', '$2b$10$mtwl873MPiL2N6vPoXa.BeGtKrdVpyixIq4vwsFNTfDrwBpgee5Wa');
